ddb062a622bc1750d5d5f10fc3ef6f5adcb33c18
Georg Walther <georgwalther@gmail.com>
2014-02-15 20:49:19 +0000

    start implementing binary search tree

--- data_structures.py
+++ data_structures.py
@@ -38,3 +38,54 @@ class HashTableArray():
         index = get_index(obj)
         self.objects[index] = None
 
+
+class BinarySearchTree():
+    """
+    Binary Search Tree that stores integers.
+    I choose integers here to keep size comparisons simple.
+    """
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.log = logging.getLogger('Binary Search Tree')
+
+    def insert(self, value):
+        if self.value == value:
+            self.log.warn('Value %d already exists in tree. '
+                          'Not inserted.' % value)
+            return
+
+        if value < self.value:
+            if self.left is not None:
+                self.left.insert(value):
+            else:
+                self.left = BinarySearchTree(value)
+        elif value > self.value:
+            if self.right is not None:
+                self.right.insert(value):
+            else:
+                self.right = BinarySearchTree(value)
+        else:
+            self.log.error('Value %d should have '
+                           'been identified at the root.' % value)
+            raise('Error inserting value %d.' % value)
+
+    def search(self, value):
+        if self.value == value:
+            return self.value
+
+        if value < self.value:
+            if self.left is not None:
+                return self.left.search(value)
+            else:
+                return None
+        elif value > self.value:
+            if self.right is not None:
+                return self.right.search(value)
+            else:
+                return None
+        else:
+            self.log.error('Value %d should have been found in root.' % value)
+            raise('Error looking for value %d.' % value)
